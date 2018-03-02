d122dd84d2a3d9aee6d154702701ef29b5d2060c
github username <github email address>
2018-03-02 15:06:03 -0500

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
 def my_select(collection)
+  if block_given?
+    i = 0
+    final_arr = Array.new
+    while i < collection.length
+    if yield(collection[i])
+      final_arr.push(collection[i])
+    end
+      i += 1
+    end
+    final_arr
+  else
+    "This block should not run!"
+  end
  # your code here!
 end
